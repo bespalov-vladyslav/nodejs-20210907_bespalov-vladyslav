d73f7b32d181f4a2df6b244813d4447fa5189b21
Bespalov Vladyslav <bespalov.vladyslav@airslate.com>
2021-09-30 21:44:34 +0300

    Stream task #1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,33 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+  constructor({limit, ...options}) {
     super(options);
+
+    this.limit = limit;
+    this.store = 0;
+  }
+
+  getSizeChunk(chunk) {
+    return Buffer.byteLength(chunk);
+  }
+
+  canReadMore(sizeChunk) {
+    const {store, limit} = this;
+    return sizeChunk + store < limit;
+  }
+
+  increaseStoreValue(value) {
+    this.store += value;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const sizeChunk = this.getSizeChunk(chunk);
+    if (this.canReadMore(sizeChunk)) {
+      this.increaseStoreValue(sizeChunk);
+      return callback(null, chunk);
+    }
+    return callback(new LimitExceededError());
   }
 }
 
